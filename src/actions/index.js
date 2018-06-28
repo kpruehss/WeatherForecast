d41c7f446181d371bf4a70bff121a35eb3cca475
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2018-06-28 18:49:24 +1000

    EDIT: API call action-creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,12 @@
 const API_KEY = 'f110df06aeeb8370cb253a321d6a0874';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+
+  return {
+    type: FETCH_WEATHER
+  };
+}
