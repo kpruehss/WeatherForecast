534dfbb064f2a6bcf7f39ed1a63f9971b696917f
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2018-06-29 12:37:44 +1000

    ADD: Chart functional component with formatting in CSS

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Sparklines, SparklinesLine  } from 'react-sparklines';
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
     const cityName = cityData.city.name;
     const temps = cityData.list.map(weather => weather.main.temp);
+    const pressure = cityData.list.map(weather => weather.main.pressure);
+    const humidity = cityData.list.map(weather => weather.main.humidity);
     
     
     return (
       <tr key={cityName}>
         <td>{cityName}</td>
-        <td>
-          <Sparklines height={120} width={180} data={temps} >
-            <SparklinesLine color="red" />
-          </Sparklines>
-        </td>
+        <td><Chart data={temps} color='red' /></td>
+        <td><Chart data={pressure} color='green' /></td>
+        <td><Chart data={humidity} color='blue' /></td>
       </tr>
     );
   }
