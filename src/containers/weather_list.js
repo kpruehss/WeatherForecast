664f7f46c275da509f8813aa216a9770f0778c3e
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2018-06-29 12:08:03 +1000

    ADD: weather_list component

---
+++ src/containers/weather_list.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class WeatherList extends Component {
+  renderWeather(cityData) {
+    const cityName = cityData.city.name;
+    
+    return (
+      <tr key={cityName}>
+        <td>{cityName}</td>
+      </tr>
+    );
+  }
+
+  render() {
+    return (
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>city</th>
+            <th>Temperature</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.weather.map(this.renderWeather)}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+function mapStateToProps({ weather }) {
+  return { weather }; // { weather } === weather: state.weather
+}
+
+export default connect(mapStateToProps)(WeatherList);
