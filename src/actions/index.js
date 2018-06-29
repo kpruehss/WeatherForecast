8d298dfa4c790e8d8679d747f8800c0c8bbbc673
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2018-06-29 10:56:53 +1000

    EDIT: Connected SearchBar container to fetchWeather action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = 'f110df06aeeb8370cb253a321d6a0874';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
